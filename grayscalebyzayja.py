3f3a9064f8d7a6f3eb152c846a804d15d6fde758
Zainab Jahan Umaima <thezainabjahan14@gmail.com>
2025-08-06 13:09:24 +0530

    Update grayscalebyzayja.py

--- grayscalebyzayja.py
+++ grayscalebyzayja.py
@@ -2,4 +2,5 @@ import cv2
 img=cv2.imread("C:/Users/Student/Pictures/lenatest.jpeg",cv2.IMREAD_GRAYSCALE)
 cv2.imshow("Lena Image",img)
 cv2.waitKey(0)
-cv2.destryAllWindows()
+
+cv2.destroyAllWindows()
